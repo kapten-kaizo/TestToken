b078bc258d7abcbb54ba8181ceb07a0872041176
KY ^ <73789857+kapten-kaizo@users.noreply.github.com>
2024-07-25 20:35:16 +0700

    Update Stoken.sol

--- Stoken.sol
+++ Stoken.sol
@@ -5,13 +5,11 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("Test Token SwissTronik", "STST")
-        Ownable(initialOwner)
-    {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+contract TestToken is ERC20 {
+    constructor(
+      string memory name,
+      string memory symbol
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, 1_000_000 * (10**18));
     }
 }
